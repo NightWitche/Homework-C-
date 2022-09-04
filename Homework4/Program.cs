d7a3a641549aa431c136f66722449ef28e2f079e
Margo <rita-1991@mail.ru>
2022-09-04 04:03:44 +0300

    Homework4

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -2,20 +2,23 @@
 //и возводит число A в натуральную степень B.
 
 Console.WriteLine("Enter number...");
-
-double ATodegree ()
+void Progressive()
 {
     Console.WriteLine("number A");
-    double numberA = Double.Parse(Console.ReadLine()); 
+    int numberA = Convert.ToInt32(Console.ReadLine()); 
     Console.WriteLine("number B");
-    double numberB = Double.Parse(Console.ReadLine());
-    double result = Math.Pow(numberA,numberB);
-    System.Console.WriteLine($"{numberA} degree {numberB}");
-    return result;
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    int result = numberA;
+   for(int i=1; i<= numberB;i++)
+   {
+       result *= numberA;
+       Console.WriteLine(result);      
+   }
+   Console.WriteLine(result);
 }
-    Console.WriteLine($"{ATodegree():f3}");*/
+Progressive();*/
 
- /*// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму 
+ /* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму 
  //цифр в числе.
 
 
@@ -35,10 +38,57 @@ Console.WriteLine("Enter number...");
 int SumNumber = SummDigits(Console.ReadLine());
 Console.WriteLine($"Summ of digits of this number is {SumNumber}");*/
 
-//Напишите программу, которая задаёт массив из введенного числа и выводит их на экран,
-//пользователь вводит число элементов в массиве и минимальное, и максимальное значение, 
-//надо заполнить массив рандоными данными от минимального до максимального значение.
-//И важное уточнениее , делать все в методе который возвращает массив.
+// Напишите программу, которая задаёт массив из введенного числа и выводит их на экран,
+// пользователь вводит число элементов в массиве и минимальное, и максимальное значение, 
+// надо заполнить массив рандоными данными от минимального до максимального значение.
+// И важное уточнениее , делать все в методе который возвращает массив.
+
+int[] MaxMinArray()
+{
+    Console.Write("Введите размер массива...");
+    int Size = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+    int[] arr = new int[Size];
+
+    Console.Write("Введите минимальное значение массива...");
+    int minNum = Convert.ToInt32(Console.ReadLine());
+    
+    Console.Write("Введите максимальное значение массива...");
+    int maxNum = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 0; i <= Size - 1; i++)
+    {
+        if (minNum <= maxNum) arr[i] = new Random().Next(minNum, maxNum + 1);
+        else arr[i] = new Random().Next(maxNum, minNum + 1);
+    }
+
+    return arr;
+}
+
+void SortAndPrintArray(int[] cheking)
+{
+    for (int i = 0; i < cheking.Length; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < cheking.Length; j++)
+        {
+            if (cheking[min] > cheking[j]) min = j;
+        }
+
+        int go = cheking[min];
+        cheking[min] = cheking[i];
+        cheking[i] = go;
+
+        if (i == cheking.Length - 1) Console.Write($"{cheking[i]} ");
+        else Console.Write($"{cheking[i]}, ");
+    }
+}
+
+int[] array = MaxMinArray();
+SortAndPrintArray(array);
+
+
+
 
 
 
