d713aa1d5d80e12f95f335332d8c10ff1b4d11be
Margo <rita-1991@mail.ru>
2022-09-07 04:25:52 +0300

    tester

---
+++ Homework6/Program.cs
@@ -0,0 +1,41 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Enter numbers...");
+int cross = Convert.Parse(Console.ReadLine());
+int[] array = new int[cross];
+int count = 0;
+for (int i=0; i<array.Length;i++)
+{
+    if(array[i]>0)
+    {
+      count++;
+    }
+    Console.WriteLine(count);
+}
+
+
+
+
+/*// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+double x = (b1 - b2)/(k1 - k2);
+double y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");*/
+
