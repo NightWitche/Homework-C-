e11dc08154b82cfcc203a8c45ffb83828345eff6
Margo <rita-1991@mail.ru>
2022-09-07 03:30:03 +0300

    Homework5

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -46,3 +46,29 @@ void NotEvenPosArray (int[] arr)
  Console.WriteLine(SummPozIndArray);
 }
 NotEvenPosArray(array);*/
+
+/*// Задайте массив вещественных чисел. Найдите разницу между максимальным 
+// и минимальным элементов массива.
+
+int[] array = new int[15];
+void MinMaxArray(int[] arr)
+{
+  double MinNum= 0;
+  double MaxNum= 0;
+  
+  for(int i = 0; i < arr.Length; i++)
+ {
+    arr[i] = new Random().Next(-5,25);
+    Console.Write(arr[i]+" ");
+ }
+   for (int i = 1; i < arr.Length; i++)
+ {
+    if (arr[i] < MinNum) MinNum = arr[i];
+    if (arr[i] > MaxNum) MaxNum = arr[i];
+     
+ }
+     Console.WriteLine($"Min number array {MinNum}\nMax number array {MaxNum}");
+     Console.WriteLine($"{MaxNum - Math.Abs(MinNum)}");
+}
+MinMaxArray(array);*/
+
